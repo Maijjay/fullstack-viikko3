9a737915b42c3e10d6823033b2c6b5f4b1a35fb5
Maijjay <maija.ylitalo95@gmail.com>
2022-02-10 18:17:19 +0200

    update

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express()
 const cors = require('cors')
 
 app.use(express.json())
-app.use(express.static('build'))
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
@@ -38,9 +38,9 @@ const makeId = () => {
 }
 
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello World!</h1>')
-// })
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
